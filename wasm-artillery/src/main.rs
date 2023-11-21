ea4f84dac4129cc020d7fcde5d38d6c6a17f0a3e
ThatBigDerp <darkhacker8604@protonmail.com>
2023-11-21 16:59:22 +0000

    Terrain data storing

--- wasm-artillery/src/main.rs
+++ wasm-artillery/src/main.rs
@@ -1,27 +1,42 @@
+use simdnoise::*;
+use bevy_math::{uvec2, URect};
+use pixel_map::PixelMap;
+use bitflags::bitflags;
 
-mod terrain_gen;
-pub use terrain_gen::Terrain;
-#[derive(PartialEq, Debug)]
-struct Pixel {
-    state: u8,
-}
+bitflags! {
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+    struct Pixel: u32 {
+        const ALIVE = 0b0000_0001;
+        const DEAD = 0b0000_0000;
+        const WAS_ALIVE = 0b0000_0100;
 
-
-impl Pixel {
-     fn new(alive: bool) -> Pixel {
-        let mut state: u8 = 0b_0000_0000 | alive as u8;
-        state = state | ((alive as u8) << 1);
-        Pixel {
-           state: state,
-        }
+        const TERRAIN = Self::ALIVE.bits() | Self::WAS_ALIVE.bits();
+        const AIR = Self::DEAD.bits();
     }
 }
 
-
 fn main() {
 
-    let terrain: Terrain::new(500,500);
+    // MOCK RANDOM TERRAIN GENERATION
+    let mut noise: Vec<u32> = vec![];
+    let max: u32 = 350;
+    let min: u32 = 250;
+    for i in 0..=100 {
+        noise.push((i as u32)+250);
+    }
+    for i in (0..100).rev() {
+        noise.push((i as u32)+250)
+    }
+    print!("{:?}", noise);
+// TERRAIN PIXEL MAP
+    let mut pixel_map = PixelMap::<Pixel>::new(
+        &uvec2(500, 500), // size of the pixel map
+        Pixel::AIR,     // initial value
+        1,                  // pixel size
+    );
+// FROM 1D NOISE TO 2D TERRAIN
+pixel_map.draw_rect(&URect::from_corners((0, 0).into(), (500, min).into()), Pixel::TERRAIN);
+for i in 0..=500 {
     
 }
-
-
+}
